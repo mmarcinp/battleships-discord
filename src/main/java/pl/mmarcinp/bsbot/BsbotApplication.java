3b1a23140b21068a00777678dba81c0031ab86b2
Marcin <marcin1przybylski@gmail.com>
2021-07-23 17:59:38 +0200

    init

---
+++ src/main/java/pl/mmarcinp/bsbot/BsbotApplication.java
@@ -0,0 +1,13 @@
+package pl.mmarcinp.bsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mmarcinp/bsbot/BsbotApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mmarcinp.bsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
